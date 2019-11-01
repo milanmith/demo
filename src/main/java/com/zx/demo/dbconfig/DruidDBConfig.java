2f64f80376fa09aa18218a5426c5715b2f2a392a
milanmith <45915151+milanmith@users.noreply.github.com>
2019-11-01 21:30:39 +0800

    first commit

--- src/main/java/com/zx/demo/dbconfig/DruidDBConfig.java
+++ src/main/java/com/zx/demo/dbconfig/DruidDBConfig.java
@@ -69,8 +69,8 @@ public class DruidDBConfig {
     @Value("{spring.datasource.connectionProperties}")
     private String connectionProperties;
 
-    @Bean     //声明其为Bean实例
-    @Primary  //在同样的DataSource中，首先使用被标注的DataSource
+    @Bean   
+    @Primary
     public DataSource dataSource(){
         DruidDataSource datasource = new DruidDataSource();
 
